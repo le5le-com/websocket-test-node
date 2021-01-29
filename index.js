359591d2169c5c06ff5991ab11bd27448e96f6c6
Alsmile <alsmile123@qq.com>
2021-01-29 11:46:19 +0800

    topology test

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var ws = require('nodejs-websocket');
 var server = ws
   .createServer(function (conn) {
     console.log('New connection');
-    conn.sendText(`{"a":123}`);
+
+    conn.sendText(
+      JSON.stringify({
+        event: 'test-color',
+        data: [
+          {
+            key: 'font.color',
+            value: 'red',
+          },
+        ],
+      })
+    );
     conn.on('text', function (str) {
       console.log('Received ' + str);
       conn.sendText(str);
